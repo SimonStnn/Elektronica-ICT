0fae2924e17999366b39292cebfe217e51d125f3
SimonStnn <simon.stijnen.23@gmail.com>
2023-12-12 15:01:03 +0100

    Make Start2Game intro to prog

---
+++ semester_1/introduction-to-programming/les12_Classes/Start2Game/Start2Game/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start2Game
+{
+	class Player
+	{
+		// ToDo 2 : Add the following properties to the Player class
+		//          - Level (int) , default value 1
+		//          - XP (int) , default value 0
+		//          - Health (int) , default value 100
+		public int level = 1;
+		public int xp = 0;
+		public int health = 100;
+
+		// 3 : Add the following methods to the Player class
+		//          - LevelUp() : increase the level by 1
+		//          - GainXP(int amount) : increase the XP by the amount
+		//          - ResetXP() : set the XP to 0
+		//          - TakeDamage(int amount) : decrease the Health by the amount, if the Health is less than 0, set it to 0
+		//          - GainHealth(int amount) : increase the Health by the amount
+		//          - IsDead() : return true if the Health is less than or equal to 0
+
+		public void LevelUp()
+		{
+			level++;
+		}
+		public void GainXP(int amount)
+		{
+			xp += amount;
+		}
+		public void ResetXP()
+		{
+			xp = 0;
+		}
+		public void TakeDamage(int amount)
+		{
+			health -= amount;
+			if(health < 0)
+				health = 0;
+		}
+		public void GainHealth(int amount)
+		{
+			health += amount;
+		}
+		public bool IsDead()
+		{
+			return health <= 0;
+		}
+	}
+}
---
+++ semester_1/introduction-to-programming/les12_Classes/Start2Game/Start2Game/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Start2Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Start2Game!");
+
+            // 1 : Create a new class called Player
+
+
+            // 4 : Create a new instance of the Player class, and assign it to the variable studentAtTheLastRow
+            Player studentAtTheLastRow = new();
+
+            // 5 : Call the GainXP method on the studentAtTheLastRow, and pass in 100
+            //          Write the Level and XP of the studentAtTheLastRow to the console
+            studentAtTheLastRow.GainXP(100);
+            Console.WriteLine($"Level: {studentAtTheLastRow.level}, XP: {studentAtTheLastRow.xp}");
+
+            // 6 : Call the TakeDamage method on the studentAtTheLastRow, and pass in 50
+            //          Write the Health of the studentAtTheLastRow to the console
+            //          Check if the studentAtTheLastRow is dead, if yes, write "Your Dead!" to the console
+            studentAtTheLastRow.TakeDamage(50);
+            Console.WriteLine($"Health: {studentAtTheLastRow.health}");
+			if(studentAtTheLastRow.IsDead())
+				Console.WriteLine("You're Dead!");
+
+			// 7 : Call the GainHealth method on the studentAtTheLastRow, and pass in 20
+			//          Write the Health of the studentAtTheLastRow to the console
+			studentAtTheLastRow.GainHealth(20);
+			Console.WriteLine($"Health: {studentAtTheLastRow.health}");
+
+            // 8 : Add 120 XP to the studentAtTheLastRow
+            //          Check if the XP > 200, if yes, call the LevelUp method and the ResetXP method on the studentAtTheLastRow
+            //          Write the Level and XP of the studentAtTheLastRow to the console
+            studentAtTheLastRow.GainXP(120);
+            if(studentAtTheLastRow.xp > 200)
+            {
+                studentAtTheLastRow.LevelUp();
+                studentAtTheLastRow.ResetXP();
+            }
+			Console.WriteLine($"Level: {studentAtTheLastRow.level}, XP: {studentAtTheLastRow.xp}");
+
+            // 9 : Call the TakeDamage method on the studentAtTheLastRow, and pass in 75
+            //          Write the Health of the studentAtTheLastRow to the console
+            //          Check if the studentAtTheLastRow is dead, if yes, write "Your Dead!" to the console
+            studentAtTheLastRow.TakeDamage(75);
+			Console.WriteLine($"Health: {studentAtTheLastRow.health}");
+
+			if(studentAtTheLastRow.IsDead())
+				Console.WriteLine("You're Dead!");
+
+			Console.WriteLine("Tanks for using Start2Game!");
+
+            // The expected output should be:
+            // Welcome to Start2Game!
+            // Your level is 1 and your XP is 100
+            // Your healt is 50
+            // Your healt is 70
+            // Your level is 2 and your XP is 0
+            // Your healt is 0
+            // Your Dead!
+            // Tanks for using Start2Game!
+        }
+    }
+}
