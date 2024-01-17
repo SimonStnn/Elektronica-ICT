c7d3d24cbc839dea0c00720c525c4e5972562984
SimonStnn <simon.stijnen.23@gmail.com>
2024-01-17 10:00:53 +0100

    Add 2nd example examen_oefening intro to prog

--- semester_1/introduction-to-programming/examen_oefening/Program.cs
+++ semester_1/introduction-to-programming/examen_oefening/Program.cs
@@ -3,8 +3,26 @@
 	return word == String.Join("", word.ToCharArray().Reverse());
 }
 
-Console.WriteLine(isPolindroom("Mom")); 
-Console.WriteLine(isPolindroom("uwu")); 
-Console.WriteLine(isPolindroom("joren")); 
+bool isPolindroom2(string word)
+{
+	string reverse = "";
+	for(int i = word.Length - 1; i >= 0; i--)
+	{
+		reverse += word[i];
+	}
+	return word == reverse;
+}
+
+Console.WriteLine("1:");
+Console.WriteLine(isPolindroom("Mom"));
+Console.WriteLine(isPolindroom("uwu"));
+Console.WriteLine(isPolindroom("joren"));
 Console.WriteLine(isPolindroom("regen"));
 Console.WriteLine(isPolindroom("godeliveradareviledog"));
+
+Console.WriteLine("2:");
+Console.WriteLine(isPolindroom2("Mom"));
+Console.WriteLine(isPolindroom2("uwu"));
+Console.WriteLine(isPolindroom2("joren"));
+Console.WriteLine(isPolindroom2("regen"));
+Console.WriteLine(isPolindroom2("godeliveradareviledog"));
