fad72ef8d20a9ddb81d79d065508132fd2f55377
SimonStnn <simon.stijnen.23@gmail.com>
2024-01-17 09:49:31 +0100

    Add examen_oefening for intro to prog

---
+++ semester_1/introduction-to-programming/examen_oefening/Program.cs
@@ -0,0 +1,10 @@
+﻿bool isPolindroom(string word)
+{
+	return word == String.Join("", word.ToCharArray().Reverse());
+}
+
+Console.WriteLine(isPolindroom("Mom")); 
+Console.WriteLine(isPolindroom("uwu")); 
+Console.WriteLine(isPolindroom("joren")); 
+Console.WriteLine(isPolindroom("regen"));
+Console.WriteLine(isPolindroom("godeliveradareviledog"));
